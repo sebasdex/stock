46bfa945fa7b01f20de19339602d8ed33c4ab803
DexterDC <sebasdc9@hotmail.com>
2024-04-01 19:02:50 -0600

    update

--- src/libs/db.js
+++ src/libs/db.js
@@ -2,10 +2,10 @@ import mysql from "serverless-mysql";
 
 export const conn = mysql({
   config: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    host: "viaduct.proxy.rlwy.net",
+    user: "root",
+    password: "devdgPMbCmcKcquqfqsWmSnPpfZVfXSy",
+    port: 53950,
+    database: "railway",
   },
 });
